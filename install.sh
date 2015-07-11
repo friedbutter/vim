3d47c9540bdc9943026a78bb1a8deeae3f84db09
Sereyvathana Ty <sereyvathana.ty@gmail.com>
2015-07-10 23:18:56 -0700

    fixed crashes with install script

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@ mkdir -p /tmp/vim_bk
 mv ~/.vimrc /tmp/vim_bk/.vimrc 2>/dev/null
 mv ~/.vim /tmp/vim_bk/.vim 2>/dev/null
 cp .vimrc ~/.vimrc
-cp -r .vim/* ~/.vim/*
-echo "Install completed"
+cp -r .vim ~/.vim
 cd ~/.vim/
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 cd $curpath
-
-
+echo "Install completed"
