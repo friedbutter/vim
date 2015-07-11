a2837ac5ca5309978d3c55b9b8d88f3e7ba079fa
Sereyvathana Ty <sereyvathana.ty@gmail.com>
2015-07-10 23:03:00 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curpath=$(pwd)
 
 mkdir -p /tmp/vim_bk
 mv ~/.vimrc /tmp/vim_bk/.vimrc 2>/dev/null
@@ -6,3 +7,8 @@ mv ~/.vim /tmp/vim_bk/.vim 2>/dev/null
 cp .vimrc ~/.vimrc
 cp -r .vim/* ~/.vim/*
 echo "Install completed"
+cd ~/.vim/
+git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+cd $curpath
+
+
